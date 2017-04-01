5ae55706c225a2125d27c7eabd619e2e1899e0d1
Imanuel Chen <imanuelhchen@gmail.com>
2017-04-01 15:58:45 -0700

    cleaning git repository

--- depopulate.py
+++
@@ -1,9 +0,0 @@
-from app import models, db
-
-models.Painting.query.delete()
-print('Paintings deleted')
-models.Category.query.delete()
-print('Categories deleted')
-models.Video.query.delete()
-print('Videos deleted')
-db.session.commit()
