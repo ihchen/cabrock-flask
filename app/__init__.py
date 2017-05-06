8859c6281208f1fd55efaa18bfcf6dc086f8f393
Imanuel Chen <imanuelhchen@gmail.com>
2017-05-06 13:47:03 -0700

    Initialized application with Flask-assets

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,5 +14,6 @@ mail = Mail(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 csrf = CSRFProtect(app)
+assets = Environment(app)
 
 from app import views, models
